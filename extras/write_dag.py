a4718d7a76063f662a40dd9bca4ba028c8da541b
Shaon Ghosh <shaonghosh@gmail.com>
2020-08-02 20:41:58 -0400

    write_dag.py now creates the required stacking subfile

--- extras/write_dag.py
+++ extras/write_dag.py
@@ -1,38 +1,32 @@
 import argparse
 
-def uber_parser():
+
+def parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-S", "--stacking", action="store_true", default=False,
-                        help="Use this option to create stacking dag")
-
-    return parser
-
-def parser_compute_evidence():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dagfile", action="store", help="Name of the output DAG file")
-    parser.add_argument("-i", "--input", action="store", help="Name of posterior file")
-    parser.add_argument("-p", "--prior", action="store", help="Name of the prior file")
-    parser.add_argument("-o", "--output", action="store", help="Prefix of the output file names")
-    parser.add_argument("-n", "--num", action="store", type=int, help="Number of jobs")
-    parser.add_argument("-t", "--target", action="store", help="Name of target EoS")
-    parser.add_argument("-r", "--reference", action="store", help="Name of reference EoS")
-    parser.add_argument("-T", "--trials", action="store", type=int, help="Number of trials")
-    parser.add_argument("-j", "--joinsubfilename", action="store", help="Name of json joining subfile")
-    parser.add_argument("-s", "--subfilename", action="store", help="Name of bayes-factor computation subfile")
-    return parser
-
-def parser_stacking():
-    parser = argparse.ArgumentParser(parents=[random_parser])
-    parser.add_argument("-d", "--dagfile", action="store", help="Name of the output DAG file")
-    parser.add_argument("--posteriorlist", action="store",
-                        help="A text file with list of posterior samles for stacking")
-    parser.add_argument("--priorlist", action="store",
-                        help="A text file with List of priors samples for stacking")
-    parser.add_argument("-o", "--output", action="store", help="Prefix of the output file names")
-    parser.add_argument("-n", "--num", action="store", type=int, help="Number of jobs")
-    parser.add_argument("-t", "--target", action="store", help="Name of target EoS")
-    parser.add_argument("-r", "--reference", action="store", help="Name of reference EoS")
-    parser.add_argument("-T", "--trials", action="store", type=int, help="Number of trials")
+    parser.add_argument("-d", "--dagfile", action="store",
+                        help="Name of the output DAG file")
+    parser.add_argument("-i", "--input", action="store", default=None,
+                        help="Name of posterior file")
+    parser.add_argument("--posteriorlist", action="store", default=None,
+                        help="Text file with list of posteriors for stacking")
+    parser.add_argument("-p", "--prior", action="store", default=None,
+                        help="Name of the prior file")
+    parser.add_argument("--priorlist", action="store", default=None,
+                        help="Text file with List of priors for stacking")
+    parser.add_argument("-o", "--output", action="store",
+                        help="Prefix of the output file names")
+    parser.add_argument("-n", "--num", action="store", type=int,
+                        help="Number of jobs")
+    parser.add_argument("-t", "--target", action="store",
+                        help="Name of target EoS")
+    parser.add_argument("-r", "--reference", action="store",
+                        help="Name of reference EoS")
+    parser.add_argument("-T", "--trials", action="store", type=int,
+                        help="Number of trials")
+    parser.add_argument("-j", "--joinsubfilename", action="store",
+                        help="Name of json joining subfile")
+    parser.add_argument("-s", "--subfilename", action="store",
+                        help="Name of bayes-factor computation subfile")
     parser.add_argument("-S", "--stacksubfilename", action="store",
                         help="Name of stacking computation subfile")
     return parser
@@ -40,9 +34,10 @@ def parser_stacking():
 
 def main(args=None):
     import os
+    import sys
 
-    P = uber_parser()
-    uber_args = P.parse_args()
+    p = parser()
+    args = p.parse_args()
 
     ## Create directories ##
     os.system("mkdir -p output")
@@ -50,27 +45,43 @@ def main(args=None):
     os.system("mkdir -p logs")
     os.system("mkdir -p {}".format(args.output))
 
-    if uber_args.stacking:
-        p = parser_stacking()
-        args = p.parse_args()
+    ## Sanity checks ##
+    # if (args.input is None) ^ (args.posteriorlist is None):
+    #     print('Either --input or --posteriorlist arguments should be provided')
+    #     sys.exit(0)
+
+
+    if args.posteriorlist:
         stack_subfile_txt = '''universe = vanilla
-executable = ./comp_stacked_evidence
-arguments = "--input {} --prior {} --target {} --reference {} --nums 40 --output {}"
-output = output_$(macrotag).stdout
-error = error_$(macrotag).stderr
-log = logfile_$(macrotag).log
-getenv = True
-accounting_group = ligo.prod.o2.cbc.pe.lalinferencerapid
-queue 1
-'''.format(posterior_files_list, prior_files_list.dat, args.target, args.reference, args.output)
+    executable = ./comp_stacked_evidence
+    arguments = "--input {} --prior {} --target {} --reference {} --nums 40 --output {}"
+    output = output_$(macrotag).stdout
+    error = error_$(macrotag).stderr
+    log = logfile_$(macrotag).log
+    getenv = True
+    accounting_group = ligo.prod.o2.cbc.pe.lalinferencerapid
+    queue 1
+    '''.format(args.posteriorlist, args.priorlist, args.target, args.reference, args.output)
 
         with open(args.stacksubfilename, 'w') as f:
             f.writelines(stack_subfile_txt)
 
-    else:
-        p = parser_compute_evidence()
-        args = p.parse_args()
+        join_subfile_txt = '''universe = vanilla
+    executable = ./combine_stacked_results
+    arguments = "--json {} --output {}"
+    output = output_$(macrotag).stdout
+    error = error_$(macrotag).stderr
+    log = logfile_$(macrotag).log
+    getenv = True
+    accounting_group = ligo.prod.o2.cbc.pe.lalinferencerapid
+    queue 1
+    '''.format(args.output, args.output)
 
+        with open(args.stacksubfilename, 'w') as f:
+            f.writelines(stack_subfile_txt)
+
+
+    if args.input:
         ### Write comp_eos_evidence sub file ###
         subfile_txt = '''universe = vanilla
 executable = ./comp_eos_evidence
@@ -116,20 +127,3 @@ queue 1
 
 if __name__ == "__main__":
     main()
-
-### Write stacking sub file ###
-#if args.posteriors:
-#    stack_subfile_txt = '''universe = vanilla
-#executable = ./comp_stacked_evidence
-#arguments = "--input $(macroposteriors) --prior (macropriorfiles) --labels $(macrolabels) --target {} --reference {} --nums {} --output $(macrooutput)"
-#output = output_$(macrotag).stdout
-#error = error_$(macrotag).stderr
-#log = logfile_$(macrotag).log
-#getenv = True
-#accounting_group = ligo.prod.o2.cbc.pe.lalinferencerapid
-#queue 1
-#'''.format(args.target, args.reference, args.trials)
-
-#    with open(args.stacksubfilename, 'w') as f:
-#        f.writelines(stack_subfile_txt)
-
